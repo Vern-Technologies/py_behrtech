e370015034b986504bc2ce8fd024f180274e4fb7
Matthew Ashley <matthewashley@verntechnologies.com>
2019-11-27 15:27:13 -0500

    Added new class and re-built package
    Added new class for Ir5VSensor and added new functions to the Parser class for getting the ids of each message, getting messaged data for a specified id, and for getting message data within a specified date range. Also re-built this package to include changes.

--- build/lib/gateway_package/Parsers/__init__.py
+++ build/lib/gateway_package/Parsers/__init__.py
@@ -1,4 +1,5 @@
 
 from gateway_package.Parsers.Synaptic import Synaptic
 from gateway_package.Parsers.EnvSensor import EnvSensor
-from gateway_package.Parsers.Ir3v3Sensor import Ir3v3Sensor
+from gateway_package.Parsers.Ir3V3Sensor import Ir3V3Sensor
+from gateway_package.Parsers.Ir5VSensor import Ir5VSensor
