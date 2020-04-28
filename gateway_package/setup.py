8bfafe98815a8dc16cd3ffe815b1b2bf7972eff6
Matthew Ashley <matthewashley1@gmail.com>
2020-04-28 16:02:23 -0400

    Testing publish action for tagged commits

--- gateway_package/setup.py
+++ gateway_package/setup.py
@@ -5,7 +5,7 @@ with open("gateway_package/README.md", "r") as fh:
 
 setuptools.setup(
     name="Fetch_BehrTech",
-    version="0.3.11",
+    version="0.3.12",
     license="MIT",
     author="Matthew Ashley",
     author_email="matthewashley1@gmail.com",
